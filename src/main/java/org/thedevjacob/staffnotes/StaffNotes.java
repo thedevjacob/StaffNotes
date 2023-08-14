7779631e535ea8ebcbe8a3dd8676ff016e74f759
thedevjacob <jasoryal@gmail.com>
2023-08-13 22:49:46 -0700

    Creation of files.

---
+++ src/main/java/org/thedevjacob/staffnotes/StaffNotes.java
@@ -0,0 +1,18 @@
+package org.thedevjacob.staffnotes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffNotes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+    }
+}
